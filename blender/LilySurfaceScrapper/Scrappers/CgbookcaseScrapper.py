bc20d4647af3b866e1c031329a52dc70a826c961
ogierm <horstikugel@gmail.com>
2019-08-26 15:21:50 +0200

    Fix detection of double_sided

--- blender/LilySurfaceScrapper/Scrappers/CgbookcaseScrapper.py
+++ blender/LilySurfaceScrapper/Scrappers/CgbookcaseScrapper.py
@@ -45,7 +45,7 @@ class CgbookcaseScrapper(AbstractScrapper):
         # Has front or back-side?
         # Checks for the "Front" text below the item name
         # Example: https://www.cgbookcase.com/textures/autumn-leaf-22
-        double_sided = html.xpath("//div[@id='view-downloadSection']/h3") is not None
+        double_sided = len(html.xpath("//div[@id='view-downloadSection']/h3")) is not 0
 
         # Get variants
         variants_data = html.xpath("//div[@class='view-downloadLinks']/div")
