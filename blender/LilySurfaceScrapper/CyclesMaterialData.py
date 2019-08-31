2ebc52d717ec4aceb14b5402c97c07705022db8b
ogierm <horstikugel@gmail.com>
2019-08-31 14:47:03 +0200

    Update to current master version (#2)
    * Make `canHandleUrl()` checking more flexible
    
    * Enable dealing with shortened down urls (like in chromes address bar)
    
    * Not working yet
    
    * Fix detection of double_sided
    
    * Fix generation of list with variants
    
    * Make it work
    
    * Use new alpha slot in principled shader
    
    Fix #18
    
    * Update blender/LilySurfaceScrapper/Scrappers/CgbookcaseScrapper.py
    
    Co-Authored-By: Elie Michel <eliemichel@users.noreply.github.com>
    
    * File- and path names
    
    * Fix a dumb typo in the translation dictionary
    
    * Handle twosided materials
    
    Fix #20
    
    * Add copy from clipboard buttons
    
    Fix #23
    
    * Change version

--- blender/LilySurfaceScrapper/CyclesMaterialData.py
+++ blender/LilySurfaceScrapper/CyclesMaterialData.py
@@ -18,14 +18,14 @@ class CyclesMaterialData(MaterialData):
         'roughness': 'Roughness',
         'metallic': 'Metallic',
         'specular': 'Specular',
-        'opacity': '<custom>',
+        'opacity': 'Alpha',
     }
 
     def loadImages(self):
         """This is not needed by createMaterial, but is called when
         create_material is false to load images anyway"""
         for map_name, img in self.maps.items():
-            if img is None or map_name not in __class__.input_tr:
+            if img is None or map_name.split("_")[0] not in __class__.input_tr:
                 continue
             getCyclesImage(img)
 
@@ -36,30 +36,44 @@ class CyclesMaterialData(MaterialData):
         links = mat.node_tree.links
         principled_mat = PrincipledBSDFWrapper(mat, is_readonly=False)
         principled = principled_mat.node_principled_bsdf
+        back_principled = None
         mat_output = principled_mat.node_out
         principled_mat.roughness = 1.0
 
         for map_name, img in self.maps.items():
-            if img is None or map_name not in __class__.input_tr:
+            if img is None or map_name.split("_")[0] not in __class__.input_tr:
                 continue
+
+            current_principled = principled
+            if map_name.endswith("_back"):
+                if back_principled is None:
+                    # Create backface principled and connect it
+                    back_principled = nodes.new(type="ShaderNodeBsdfPrincipled")
+                    geometry_node = nodes.new(type="ShaderNodeNewGeometry")
+                    mix_node = nodes.new(type="ShaderNodeMixShader")
+                    back_principled.inputs["Roughness"].default_value = 1.0
+                    links.new(geometry_node.outputs["Backfacing"], mix_node.inputs[0])
+                    links.new(principled.outputs[0], mix_node.inputs[1])
+                    links.new(back_principled.outputs[0], mix_node.inputs[2])
+                    links.new(mix_node.outputs[0], mat_output.inputs[0])
+                current_principled = back_principled
+                map_name = map_name[:-5]  # remove "_back"
+                
             texture_node = nodes.new(type="ShaderNodeTexImage")
             texture_node.image = getCyclesImage(img)
             texture_node.image.colorspace_settings.name = "sRGB" if map_name == "baseColor" else "Non-Color"
+
             if hasattr(texture_node, "color_space"):
                 texture_node.color_space = "COLOR" if map_name == "baseColor" else "NONE"
-            if map_name == "opacity":
-                transparence_node = nodes.new(type="ShaderNodeBsdfTransparent")
-                mix_node = nodes.new(type="ShaderNodeMixShader")
-                links.new(texture_node.outputs["Color"], mix_node.inputs[0])
-                links.new(transparence_node.outputs[0], mix_node.inputs[1])
-                links.new(principled.outputs[0], mix_node.inputs[2])
-                links.new(mix_node.outputs[0], mat_output.inputs[0])
             elif map_name == "normal":
                 normal_node = nodes.new(type="ShaderNodeNormalMap")
                 links.new(texture_node.outputs["Color"], normal_node.inputs["Color"])
-                links.new(normal_node.outputs["Normal"], principled.inputs["Normal"])
+                links.new(normal_node.outputs["Normal"], current_principled.inputs["Normal"])
             else:
-                links.new(texture_node.outputs["Color"], principled.inputs[__class__.input_tr[map_name]])
+                links.new(texture_node.outputs["Color"], current_principled.inputs[__class__.input_tr[map_name]])
+
+            if map_name == "opacity":
+                mat.blend_method = 'BLEND'
 
         autoAlignNodes(mat_output)
 
