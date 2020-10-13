1a5a3141ffe6e2dd43201ad5ef8beadabbd6805a
Elie Michel <eliemichel@users.noreply.github.com>
2020-10-13 10:04:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2019-2020 Elie Michel
 #
-# This file is part of LilySurfaceScrapper, a Blender add-on to import
+# This file is part of LilySurfaceScraper, a Blender add-on to import
 # materials from a single URL. It is released under the terms of the GPLv3
 # license. See the LICENSE.md file for the full text.
 
 bl_info = {
-    "name": "Lily Surface Scrapper",
+    "name": "Lily Surface Scraper",
     "author": "Élie Michel <elie.michel@exppad.com>",
     "version": (1, 5, 1),
     "blender": (2, 82, 0),
@@ -13,13 +13,13 @@ bl_info = {
     "description": "Import material from a single URL",
     "warning": "",
     "wiki_url": "",
-    "tracker_url": "https://github.com/eliemichel/LilySurfaceScrapper/issues",
+    "tracker_url": "https://github.com/eliemichel/LilySurfaceScraper/issues",
     "support": "COMMUNITY",
     "category": "Import",
 }
 
 # A bit of boilerplate because this add-on is intended to be zipped with only
-# the blender/LilySurfaceScrapper directory, not the root of the repo
+# the blender/LilySurfaceScraper directory, not the root of the repo
 # (preferably don't use the "Download as zip" button from GitHub, but rather
 # download zip files from the release pages.
 import os
