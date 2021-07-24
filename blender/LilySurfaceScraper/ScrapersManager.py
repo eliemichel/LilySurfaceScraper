828818d63e7ec09a21f3924dcb0717d49a07f8e2
zivoy <ziv132@gmail.com>
2021-07-24 00:55:44 -0400

    update to copyright

--- blender/LilySurfaceScraper/ScrapersManager.py
+++ blender/LilySurfaceScraper/ScrapersManager.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2019 - 2020 Elie Michel
+# Edited in 2021 by Zivoy
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the “Software”), to deal
